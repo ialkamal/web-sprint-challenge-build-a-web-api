e1b0365bd7bfa2ed9b7d91e37e474564af04ac62
Ismail Al Kamal <ismail@nawatt.com>
2021-01-23 04:39:11 +0300

    Stretch complete

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config();
 const actionsRouter = require("./actions/actions-router");
 const projectsRouter = require("./projects/projects-router");
 const server = express();
@@ -6,13 +7,15 @@ const server = express();
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
 
+const MESSAGE = process.env.MESSAGE;
+
 server.use(express.json());
 
 server.use("/api/actions", actionsRouter);
 server.use("/api/projects", projectsRouter);
 
 server.use("/", (req, res) => {
-  res.status(200).send("<h1>Welcome to the Node Sprint Challenge!</h1>");
+  res.status(200).send(`<h1>${MESSAGE}</h1>`);
 });
 
 server.use((error, req, res, next) => {
